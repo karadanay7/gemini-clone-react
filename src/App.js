c7571345cbae40306c5f8e9f323855dab7b605f5
Aysegul <karadan.ay7@gmail.com>
2024-10-12 16:32:59 +0300

    layout updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+import Main from './components/Main/Main';
 
 function App() {
   return (
-    <>
+    <div className='flex'>
     <Sidebar/>
-    </>
+    <Main/>
+    </div>
   );
 }
 
