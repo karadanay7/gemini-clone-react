f3b5ff29a63d71836d65edcdd42d1a7a3e5f3500
Aysegul <karadan.ay7@gmail.com>
2024-10-12 19:16:45 +0300

    Update Header.jsx

--- src/components/Main/Header.jsx
+++ src/components/Main/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="w-full flex h-[74px] items-center px-4 leading-5 bg-white justify-between ">
             <div className=" flex items-center text-[20px] font-[300] tracking-wide gap-2 hover:bg-gray-200 py-2.5 px-3 rounded-lg ">
              
-                 <span>Gemini</span><IoMdArrowDropdown className="mt-[1px] text-[18px]" />
+                 <span >Gemini</span><IoMdArrowDropdown className="mt-[1px] text-[18px]" />
             </div>
             <div className=" flex items-center gap-3">
                 <div className="flex items-center bg-gray-200 px-6 py-2 rounded-lg text-[12px] gap-2">
