4363cee1599093e286ba36d4896f23751b7b6fcc
Aysegul <karadan.ay7@gmail.com>
2024-10-13 16:50:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+
 import Main from './components/Main/Main';
+import { useMediaQuery } from "react-responsive";
 
 function App() {
+
   return (
     <div className='flex'>
-    <Sidebar/>
+   <Sidebar /> 
+  
     <Main/>
     </div>
   );
