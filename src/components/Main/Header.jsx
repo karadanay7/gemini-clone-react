157c0c0ebc682f7ee1f694bbd660b8862ae95c6c
Aysegul <karadan.ay7@gmail.com>
2024-10-13 13:57:08 +0300

    css updated for header and main

--- src/components/Main/Header.jsx
+++ src/components/Main/Header.jsx
@@ -1,16 +1,17 @@
 import { IoMdArrowDropdown } from "react-icons/io";
 import { PiStarFourFill } from "react-icons/pi";
 import { TbGridDots } from "react-icons/tb";
+import { SiGooglegemini } from "react-icons/si";
 const Header = () => {
     return (
-        <div className="w-full flex h-[74px] items-center px-4 leading-5 bg-white justify-between ">
+        <div className="w-full  flex h-[74px] items-center px-4 leading-5 bg-white justify-between ">
             <div className=" flex items-center text-[20px] font-[300] tracking-wide gap-2 hover:bg-gray-200 py-2.5 px-3 rounded-lg ">
              
-                 <span >Gemini</span><IoMdArrowDropdown className="mt-[1px] text-[18px]" />
+                 <span >Gemini</span><IoMdArrowDropdown className="mt-[1px] text-[16px]" />
             </div>
             <div className=" flex items-center gap-3">
                 <div className="flex items-center bg-gray-200 px-6 py-2 rounded-lg text-[12px] gap-2">
-                <PiStarFourFill className="text-amber-600 text-[18px]" /> <p>Try Gemini Advanced</p>
+                <SiGooglegemini  className="text-amber-600 text-[18px]" /> <p>Try Gemini Advanced</p>
                 </div>
                 <div className="rounded-full p-2 hover:bg-gray-100">
                 <TbGridDots className="text-gray-800 text-[20px]" />
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,16 +5,18 @@ import { IoMicOutline } from "react-icons/io5";
 import { BiSend } from "react-icons/bi";
 import { Context } from "../../context/Context";
 
+
 const Main = () => {
   const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context);
-  const isLoading = true;
+ 
 
   return (
-    <div className=" grid grid-rows-[74px,1fr,min-content] justify-center h-screen  w-full text-center ">
+    <main className="grow">
       <Header />
+    <div className=" grid grid-rows-[1fr,min-content] justify-center h-[calc(100vh_-_74px)]  w-full text-center ">
 
       
-      <div className=" overflow-auto text-left p-6 h-full max-w-[840px]">
+      <article className=" overflow-auto text-left p-6 h-full max-w-4xl">
         {!showResult ? (
           <div className="place-content-center h-full gap-6">
             <div className="text-center text-[28px] font-medium">
@@ -24,12 +26,12 @@ const Main = () => {
               <p className="text-gray-800">How can I help?</p>
             </div>
 
-            <div className="text-left max-w-[840px] mx-auto p-6 bg-gray-100 rounded-xl gap-2 text-sm mt-4">
+            <div className="text-left max-w-4xl mx-auto p-6 bg-gray-100 rounded-xl gap-2 text-md mt-6">
               <div className="ml-2">
                 Humans review some saved chats to improve Google AI. To stop this for future chats, turn off Gemini Apps Activity. If this setting is on, don't enter info you wouldn’t want reviewed or used. <a href="#">How it works.</a>
               </div>
 
-              <div className="flex items-center justify-end text-sm text-blue-700 gap-3">
+              <div className="flex items-center justify-end text-sm text-blue-700 gap-6 mt-4">
                 <button>Manage Activity</button>
                 <button>Dismiss</button>
               </div>
@@ -44,10 +46,10 @@ const Main = () => {
               </p>
             </div>
 
-            <div className="flex items-start space-x-4 max-w-[664px]">
+            <div className="flex items-start space-x-4 w-full">
               <img src="/gemini.png" alt="Gemini" className="h-10 w-10" />
               <div className="text-gray-800 p-4 rounded-xl w-full overflow-hidden">
-                {isLoading ? (
+                {loading ? (
                   <div className=" space-y-2">
                     <hr className="w-full h-3 rounded-sm bg-gradient-to-r from-blue-600 via-blue-50 to-blue-300 bg-200% animate-gradientMove" />
                     <hr className="w-full h-3 rounded-sm bg-gradient-to-r from-blue-600 via-blue-50 to-blue-300 bg-200% animate-gradientMove" />
@@ -60,18 +62,18 @@ const Main = () => {
             </div>
           </div>
         )}
-      </div>
+      </article>
      
 
       
-      <footer className="max-w-[840px]">
-      <div className="flex items-center justify-center  w-full mx-auto sticky bottom-0 h-16 bg-gray-100 rounded-full p-2">
+      <footer className="max-w-4xl px-6">
+      <div className="flex items-center justify-center  w-full mx-auto sticky bottom-0 h-16 bg-gray-100 rounded-full py-2 px-6 focus-within:bg-gray-200 ">
         <input
           type="text"
           value={input}
           placeholder="Enter a prompt here"
           onChange={(e) => setInput(e.target.value)}
-          className="flex-grow bg-transparent outline-none px-4"
+          className="flex-grow bg-transparent outline-none  "
         />
         <div className="flex items-center gap-3 text-2xl text-gray-800">
           <LuImagePlus />
@@ -87,6 +89,7 @@ const Main = () => {
     
      
     </div>
+    </main>
   );
 };
 
